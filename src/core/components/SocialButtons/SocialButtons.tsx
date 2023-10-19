c0f521dbe132b23d7ebb1db12434b93ed8ce5a81
colburncodes <colburnsanders@gmail.com>
2023-10-18 21:47:14 -0500

    refactor: basic auth form

---
+++ src/core/components/SocialButtons/SocialButtons.tsx
@@ -0,0 +1,50 @@
+import { Button, ButtonProps, Group } from "@mantine/core"
+import { GithubIcon, DiscordIcon, TwitterIcon } from "@mantine/ds"
+import { GoogleIcon } from "./GoogleIcon"
+import { FacebookIcon } from "./FacebookIcon"
+
+export function GoogleButton(props: ButtonProps & React.ComponentPropsWithoutRef<"button">) {
+  return <Button leftSection={<GoogleIcon />} variant="default" {...props} />
+}
+
+export function FacebookButton(props: ButtonProps & React.ComponentPropsWithoutRef<"button">) {
+  return <Button leftSection={<FacebookIcon />} {...props} />
+}
+
+export function DiscordButton(props: ButtonProps & React.ComponentPropsWithoutRef<"button">) {
+  return (
+    <Button leftSection={<DiscordIcon style={{ width: "1rem", height: "1rem" }} />} {...props} />
+  )
+}
+
+// Twitter button as anchor
+export function TwitterButton(props: ButtonProps & React.ComponentPropsWithoutRef<"a">) {
+  return (
+    <Button
+      component="a"
+      leftSection={<TwitterIcon style={{ width: "1rem", height: "1rem" }} color="#00ACEE" />}
+      variant="default"
+      {...props}
+    />
+  )
+}
+
+export function GithubButton(props: ButtonProps & React.ComponentPropsWithoutRef<"button">) {
+  return (
+    <Button {...props} leftSection={<GithubIcon style={{ width: "1rem", height: "1rem" }} />} />
+  )
+}
+
+export function SocialButtons() {
+  return (
+    <Group justify="center" p="md">
+      <GoogleButton>Continue with Google</GoogleButton>
+      <TwitterButton href="https://twitter.com/mantinedev" target="_blank">
+        Follow on Twitter
+      </TwitterButton>
+      <FacebookButton>Sign in with Facebook</FacebookButton>
+      <GithubButton>Login with GitHub</GithubButton>
+      <DiscordButton>Join Discord community</DiscordButton>
+    </Group>
+  )
+}
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -2,13 +2,29 @@ import { Suspense } from "react"
 import Layout from "src/core/layouts/Layout"
 import { BlitzPage } from "@blitzjs/next"
 import { UserInfo } from "../core/components/UserInfo"
+import { AuthenticationForm } from "../core/components/AuthForm"
+import { useCurrentUser } from "../features/users/hooks/useCurrentUser"
+import { Stack } from "@mantine/core"
 
 const Home: BlitzPage = () => {
+  const currentUser = useCurrentUser()
   return (
     <Layout title="Home">
-      <Suspense fallback="Loading...">
-          <UserInfo />
-      </Suspense>
+      {currentUser && <UserInfo />}
+      {!currentUser && (
+        <div
+          style={{
+            display: "flex",
+            position: "relative",
+            marginLeft: 70,
+            marginTop: 50,
+          }}
+        >
+          <Stack>
+            <AuthenticationForm />
+          </Stack>
+        </div>
+      )}
     </Layout>
   )
 }
