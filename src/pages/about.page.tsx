4dedfb9430c263560663a5d00e69733e142d4843
colburncodes <colburnsanders@gmail.com>
2023-11-27 10:21:27 -0600

    about page

--- src/pages/about.page.tsx
+++ src/pages/about.page.tsx
@@ -2,6 +2,7 @@ import { BlitzPage } from "@blitzjs/next"
 import Layout from "src/core/layouts/Layout"
 
 const AboutPage: BlitzPage = () => {
+  {/* @ts-expect-error Server Component */}
   return <Layout>About Page</Layout>
 }
 
