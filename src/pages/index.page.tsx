b28fe5ab1c1f664f9105aa818bdf21dfc9c3cdb8
colburncodes <colburnsanders@gmail.com>
2023-12-05 22:31:20 -0600

    fix build errors

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -2,25 +2,14 @@ import Layout from "src/core/layouts/Layout"
 import { BlitzPage } from "@blitzjs/next"
 import { AuthenticationForm } from "../core/components/AuthForm"
 import { useCurrentUser } from "../features/users/hooks/useCurrentUser"
-import { Button, Group, Stack } from "@mantine/core"
-import { useMutation } from "@blitzjs/rpc"
-import sendTestEmail from "~/src/features/users/mutations/sendTestEmail"
-
+import { Stack } from "@mantine/core"
 const Home: BlitzPage = () => {
   const user = useCurrentUser()
-  const [$sendTestEmail] = useMutation(sendTestEmail)
 
   return (
     <>
       {/* @ts-expect-error Server Component */}
       <Layout title="Home">
-        {/* TODO: CREATE A LANDING PAGE.*/}
-        {user && <Group><Button onClick={async () => {
-          await $sendTestEmail({
-            subject: "Test email",
-            to: "colburnsanders@gmail.com"
-          })
-        }}>Send Email</Button></Group>  }
         {!user && (
           <Stack>
             <AuthenticationForm />
