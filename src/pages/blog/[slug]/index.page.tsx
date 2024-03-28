0ae4dd56743d31357df87e9268835d977e42b767
colburncodes <colburnsanders@gmail.com>
2024-03-27 21:59:22 -0500

    contentlayer configured

--- src/pages/blog/[slug]/index.page.tsx
+++ src/pages/blog/[slug]/index.page.tsx
@@ -1,19 +1,19 @@
 import React from "react"
-import { Text } from "@mantine/core"
+import { Title } from "@mantine/core"
 import Layout from "~/src/core/layouts/layout"
 import { useStringParam } from "~/src/utils/utils"
 import { BlitzPage } from "@blitzjs/next"
 
 export const BlogPostPage: BlitzPage = () => {
-  const username = useStringParam("username")
-  console.log(username)
+  const slug = useStringParam("slug")
+
   return (
     <>
       {/* @ts-expect-error Server Component */}
       <Layout>
-        <Text>
-          Hello from BlogPost
-        </Text>
+        <Title>
+          {slug}
+        </Title>
       </Layout>
     </>
   )
