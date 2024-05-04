71d5d8888aa3f13466f5156cd5bbe5853f6d37d5
colburncodes <colburnsanders@gmail.com>
2024-05-03 21:31:30 -0500

    errorhandling, user feedback, bug fix update password

--- src/pages/settings/components/change-password.tsx
+++ src/pages/settings/components/change-password.tsx
@@ -3,10 +3,10 @@ import { useForm, zodResolver } from "@mantine/form"
 import { ChangePasswordInput, ChangePasswordInputType } from "~/src/features/auth/schemas"
 import { useMutation } from "@blitzjs/rpc"
 import changePasswordLoggedInUser from "~/src/features/auth/mutations/changePasswordLoggedInUser"
+import { showNotification } from "@mantine/notifications"
 
 
 export const ChangePassword = () => {
-
   const [$changePasswordLoggedInUser, {isLoading, isSuccess}] = useMutation(changePasswordLoggedInUser)
 
   const form = useForm<ChangePasswordInputType>({
@@ -15,56 +15,72 @@ export const ChangePassword = () => {
       newPassword: "",
       newPasswordConfirmation: "",
     },
+    validate: zodResolver(ChangePasswordInput),
     validateInputOnBlur: true,
-    validate: zodResolver(ChangePasswordInput)
   })
 
-  const onSubmit = async (values: any) => {
-    await $changePasswordLoggedInUser({ ...values })
+  const onSubmit = async (values: ChangePasswordInputType) => {
+    const result = ChangePasswordInput.safeParse(values)
+    if (result.success) {
+      await $changePasswordLoggedInUser({ ...result.data })
+      form.reset()
+    } else {
+      form.setErrors(result.error.flatten().fieldErrors)
+      showNotification({
+        color: 'red',
+        title: "Error!",
+        message: 'Failed to update password. Please check the input fields.'
+      })
+    }
+    showNotification({
+      color: 'green',
+      title: "Success!",
+      message: 'Password updated'
+    })
   }
 
   return(
-    <Card withBorder w={"100%"} maw={300} ml={"20"}>
-      {isSuccess && <Text>Password successfully updated!</Text>}
-      {!isSuccess && <Group>
-        <Title order={4}>
-          Change Password
-        </Title>
-        <Group w={"100%"}>
-          <form onSubmit={form.onSubmit(onSubmit)}>
-            <PasswordInput
-              w={200}
-              size={"sm"}
-              withAsterisk
-              label="Current Password"
-              placeholder={"current password"}
-              {...form.getInputProps("currentPassword")} />
+    <>
+      <Card withBorder w={"100%"} maw={300} ml={"20"}>
+        <Group>
+          <Title order={4}>
+            Change Password
+          </Title>
+          <Group w={"100%"}>
+            <form onSubmit={form.onSubmit(onSubmit)}>
+              <PasswordInput
+                w={200}
+                size={"sm"}
+                withAsterisk
+                label="Current Password"
+                placeholder={"current password"}
+                {...form.getInputProps("currentPassword")} />
 
-            <PasswordInput
-              mt={5}
-              w={200}
-              size={"sm"}
-              withAsterisk
-              label="New Password"
-              placeholder={"new password"}
-              {...form.getInputProps("newPassword")} />
+              <PasswordInput
+                mt={5}
+                w={200}
+                size={"sm"}
+                withAsterisk
+                label="New Password"
+                placeholder={"new password"}
+                {...form.getInputProps("newPassword")} />
 
-            <PasswordInput
-              w={200}
-              mt={5}
-              withAsterisk
-              size={"sm"}
-              label="Confirm Password"
-              placeholder={"confirm password"}
-              {...form.getInputProps("newPasswordConfirmation")}
-            />
-
-          </form>
-          <Group justify="flex-start" mt="md">
-            <Button size={"xs"} loading={isLoading} disabled={!form.isValid()} type="submit">Update password</Button>
+              <PasswordInput
+                w={200}
+                mt={5}
+                withAsterisk
+                size={"sm"}
+                label="Confirm Password"
+                placeholder={"confirm password"}
+                {...form.getInputProps("newPasswordConfirmation")}
+              />
+              <Group justify="flex-start" mt="md">
+                <Button size={"xs"} loading={isLoading} disabled={!form.isValid()} type="submit">Update password</Button>
+              </Group>
+            </form>
           </Group>
         </Group>
-      </Group>}
-    </Card>
+      </Card>
+    </>
   )
 }
