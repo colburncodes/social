eb8b246b2023f3d85d96e5056caf282635b6cb91
colburncodes <colburnsanders@gmail.com>
2024-06-08 21:22:42 -0500

    update usage with mantine emotion

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,18 +1,32 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import { ColorSchemeScript } from '@mantine/core';
+import { emotionCache } from '../emotion/cache';
+import createEmotionServer from '@emotion/server/create-instance';
+import {createGetInitialProps} from "@mantine/emotion";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <ColorSchemeScript defaultColorScheme="auto" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
-      </Head>
-      <body>
-      <Main />
-      <NextScript />
-      </body>
-    </Html>
-  );
+const stylesServer = createEmotionServer(emotionCache);
+
+export default function Document  () {
+    return (
+        <Html lang="en">
+            <Head>
+                <ColorSchemeScript defaultColorScheme="auto" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
+            </Head>
+            <body>
+            <Main />
+            <NextScript />
+            </body>
+        </Html>
+    );
 }
+
+Document.getInitialProps = createGetInitialProps(
+    NextDocument,
+    stylesServer
+);
+
+
+
+
