a2135d47d67e1494f4a8f07952e2351ec676a585
colburncodes <colburnsanders@gmail.com>
2024-06-16 14:10:30 -0500

    capturing user github profileId

--- src/pages/api/auth/[...auth].page.ts
+++ src/pages/api/auth/[...auth].page.ts
@@ -4,6 +4,7 @@ import { api } from "src/blitz-server"
 import { Strategy as GitHubStrategy, Profile } from "passport-github2";
 import {env} from "~/env.mjs";
 import {URL_ORIGIN} from "~/src/config";
+import db from "~/db";
 
 
 export default api(
@@ -17,12 +18,30 @@ export default api(
                     clientSecret: env.NEXT_PUBLIC_GITHUB_AUTH_SECRET_KEY,
                     callbackURL: `${URL_ORIGIN}/api/auth/github/callback`,
                     scope: ["user:email"]
-                },
+                }, // Provide initialized passport strategy here
                 async (accessToken, refreshToken, profile: Profile, done: any) => {
-                    const email = profile.emails && profile.emails?.[0].value;
+                    const email = profile.emails?.[0].value;
                     if (!email) return done(new Error("Email not found in Github Account."));
-                    console.log(email);
-                }), // Provide initialized passport strategy here
+
+                    // TODO: create function to find & connect user with GithubID
+
+                    const user = await db.user.upsert({
+                        where: { email },
+                        create: {
+                            email,
+                            name: profile.displayName,
+                            githubProfileId: profile.id
+                        },
+                        update: { email },
+                    })
+
+                    const publicData = {
+                        userId: user.id,
+                        roles: [user.role],
+                        source: "Github",
+                    }
+                    done(undefined, { publicData, redirectUrl: '/edit-profile'  })
+                }),
             },
         ],
     })
